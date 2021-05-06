3c6e1e146545aa832ac9469dda9b78a54baa8e17
Henri Spaan <h.r.a.m.spaan@student.tudelft.nl>
2021-05-06 15:21:58 +0200

    Added runway formulas

--- tool.py
+++ tool.py
@@ -1,4 +1,5 @@
 from math import *
+import numpy as np
 
 #Optimum lift coefficient, Cl_opt
 def calculate_cl_opt(cd_0, A, e):
@@ -37,5 +38,30 @@ def calculate_max_climb_rate(p_max, W, S, cd_0, rho, A, e):
 def calculate_climb_gradient(c, v):
     """Calculates the climb gradient, G. Inputs are climb rate c and velocity v."""
     return c / v
+
+#Runway length take-off
+def calculate_runway_length_takeoff(v_final, v_initial, T, W, mu, rho, S, cl_takeoff, cd_0, A, e):
+    """Calculates the runway length for take-off, s_g. Inputs are final velocity v_final, initial velocity v_initial,
+    thrust T, weight W, ground friction constant mu, air density rho, wing surface area S, lift coefficient at take-off
+    cl_takeoff, zero lift drag coefficient cd_0, aspect ratio A and Oswald efficiency factor e."""
+    k = 1 / (pi * A * e)
+    k_t = (T / W) - mu
+    k_a = (rho / (2 * (W / S))) * (mu * cl_takeoff - cd_0 - k * cl_takeoff**2)
+    safety_margin = 1.15
+    return (1 / (2 * 9.81 * k_a)) * np.log(((k_t + k_a * v_final**2) / (k_t + k_a * v_initial**2))) * safety_margin
+
+#Take-off parameter
+def calculate_takeoff_parameter(W, S, p_max, cl_takeoff):
+    """Calculates the take-off parameter, TOP_prop. Inputs are weight W, wing surface area S, maximum power p_max and
+    lift coefficient at take-off cl_takeoff."""
+    sigma = 1
+    return (W / S) * (W / p_max) * (1 / cl_takeoff) * (1 / sigma)
+
+#Runway length landing
+def calculate_runway_length_landing(W, rho, A, cl_max):
+    """Calculates the runway length for landing, s_l. Inputs are weight W, air density rho, aspect ratio A and maximum
+    lift coefficient cl_max."""
+    v_stall = sqrt((2 * W) / rho * A * cl_max)
+    return 0.5915 * v_stall**2
 
 
