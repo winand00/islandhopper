f98b18735eb7168a900c2e0e5c44f6ea3b76ec82
Henri Spaan <h.r.a.m.spaan@student.tudelft.nl>
2021-05-06 14:52:03 +0200

    Added range and climb equations

--- tool.py
+++ tool.py
@@ -19,6 +19,23 @@ def calculate_p_cruise(W, cd_0, rho, v_cruise, S, A, e):
     Oswald efficiency factor e."""
     return W * (((cd_0 * (1 / 2) * rho * v_cruise**3) / (W / S)) + ((W / S) * (1 / pi * A * e * (1 / 2) * rho *
                                                                                v_cruise)))
+#Range
+def calculate_range(specific_energy, m_energy, m, L, D, efficiency_total):
+    """Calculates the range, R. Inputs are specific energy of the energy source E^*, mass of the energy source m_energy,
+    mass of the aircraft m, lift L, drag D and total efficiency eta_total."""
+    return specific_energy * (m_energy / m) * (1 / 9.81) * (L / D) * efficiency_total
 
+#Climb rate
+def calculate_max_climb_rate(p_max, W, S, cd_0, rho, A, e):
+    """Calculates the maximum climb rate, c. Inputs are maximum power p_max, weight W, wing surface area S, zero lift
+    drag coefficient Cd_0, air density rho, aspect ratio A and Oswald efficiency factor e."""
+    cl = sqrt(3 * cd_0 * pi * A * e)
+    cd = 4 * cd_0
+    return (p_max / W) - ((sqrt(W / S) * sqrt(2)) / ((cl**(3 / 2) / cd) * sqrt(rho)))
+
+#Climb gradient
+def calculate_climb_gradient(c, v):
+    """Calculates the climb gradient, G. Inputs are climb rate c and velocity v."""
+    return c / v
 
 
