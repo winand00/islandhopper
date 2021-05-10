2969a04a716fd3628c3f7d68b219dbec2c6a3c6a
Tjeu Gehlen <t.a.gehlen@studen.tudelft.nl>
2021-05-10 12:28:01 +0200

    DCL,DCDO written

--- distrubuted_prop.py
+++ distrubuted_prop.py
@@ -8,7 +8,7 @@ def normalized_D_p(N,dY, dy,A,W_S):
     return dY**2/(N**2*(1+dy)**2) * A/(W_S)
 
 def thrust_coef(T_W,N,rho, V, D_normalized, chi):
-    return chi*T_W/(rho*V**2*D_normalized)
+    return chi*T_W/(rho*V**2*D_normalized*N)
 
 def a_p(T_c):
     return 0.5*(sqrt(1+8*T_c/pi)-1)
@@ -24,4 +24,15 @@ def alpha_w(M,A,sweep, CL):
 
 def dcl(a_w,alpha_w, alpha_p, beta):
     return 2*pi*((sin(alpha_w)-a_w*beta*sin(alpha_p-alpha_w))*sqrt((a_w*beta)**2 + 2*a_w*beta*cos(alpha_p) + 1) - sin(alpha_w))
+
+
+
+
+
+def DCL(dY, T_c,M,A,sweep,CL,alpha_p,beta,xp_rp, T_W,rho,V,N,dy,W_S,chi):
+    return dcl(a_w(a_p(T_c),contraction_ratio(a_p(thrust_coef(T_W,N,rho, V,normalized_D_p(N,dY, dy,A,W_S), chi)),xp_rp)), alpha_w(M,A,sweep,CL), alpha_p, beta)*dY
+
+def DCD0(dY, T_c,xp_rp,cf, T_W,rho,V,N,dy,W_S,chi,A):
+    return dY*a_w(a_p(T_c),contraction_ratio(a_p(thrust_coef(T_W,N,rho, V, normalized_D_p(N,dY, dy,A,W_S), chi)),xp_rp))**2*cf
+
 
