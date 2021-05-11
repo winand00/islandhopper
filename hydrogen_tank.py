aa950365133a84456e38312b2819b80a194b4e76
dvlot <d.d.p.vlot@student.tudelft.nl>
2021-05-11 13:31:51 +0200

    Updated hydrogen

--- hydrogen_tank.py
+++ hydrogen_tank.py
@@ -1,5 +1,5 @@
 import numpy as np
-eta_fc = 0.5
+eta_fc = 0.6
 eta_total = 0.85 * eta_fc * 0.9 * 0.9
 
 LD = 9.79
@@ -12,13 +12,13 @@ payload_range = 1.852 * 200 *10**3
 rho_liquid = 70
 rho_pressure = 40
 energy_margin = 1.3
-eta_storage_liquid = 0.20
+eta_storage_liquid = 0.25
 eta_storage_pressure = 0.1
-max_power = 500 * 10**3
+max_power = 1500 * 10**3
 cruise_power = 500 * 10**3
 rho_pmad = 10000
 rho_em = 5000
-rho_fc = 2000 #3400
+rho_fc = 3400 #3400
 rho_comp = 2000
 rho_cool = 2000
 T_air = 298.15
