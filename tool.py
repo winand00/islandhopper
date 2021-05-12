1af232b1f26e951f6fb232980c078bed09534de4
timokoning <tjwk@xs4all.nl>
2021-05-12 10:06:12 +0200

    Added loitering speed

--- tool.py
+++ tool.py
@@ -156,10 +156,10 @@ def tool(cd_0, A, e, W, rho, rho_sealevel, S, specific_energy, m_energy, m, L_ov
     print(f"**************CRUISE CHARACTERISTICS******************** \n"
           f"Optimum lift coefficient Cl_opt = {round(cl_opt, 2)} [-]\n"
           f"Lift coefficient for endurance Cl_end = {round(cl_end, 2)} [-]\n"
-          f"Loitering speed = {round(v_end, 2)} [-]\n"
           f"Cruise speed                    = {round(v_cruise, 2)} [m/s] \n"
+          f"Loitering speed                    = {round(v_end, 2)} [m/s] \n"
           #f"Required cruise power           = {round(p_cruise, 2)} [Watt] \n"
-          f"Max range                       = {round(max_range, 2)} [m]")
+          f"Max range                      = {round(max_range, 2)} [m]")
 
     max_climb_rate, max_climb_gradient = calculate_max_climb_rate_and_gradient(p_max, W, S, cd_0, rho_sealevel, A, e,efficiency_prop)
     #runway_length_takeoff = calculate_runway_length_takeoff(v_final, v_initial, T, W, mu, rho, S, cl_takeoff, cd_0, A, e)
