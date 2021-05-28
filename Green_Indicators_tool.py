f5f4fd2df42b60a1cf4cee5c46716db361d11433
sgwulffraat <stijn.wulffraat@gmail.com>
2021-05-28 17:23:09 +0200

    update tool

--- Green_Indicators_tool.py
+++ Green_Indicators_tool.py
@@ -9,13 +9,16 @@ import matplotlib.pyplot as plt
 
 # GI1: Design efficiency #
 
-class Parameters1:
+class Parameters:
     def __init__(self):
+        #General Parameters
+        self.MTOW = 8618        # Maximum Take-off Weight [kg]
+
+        #Efficiency Parameters
         self.CL = 1.3           #CL [-] (Generally use cruise conditions)
         self.C_D_0 = 0.03       #CD_0 [-]
         self.A = 10             #Aspect ratio [-]
         self.e = 0.8            #Oswald effiency [-]
-        self.MTOW = 8618        #Maximum Take-off Weight [kg]
         self.dW = 1             #Extra added weight [kg] (use negative value for weight reduction)
         self.G = 4.73           #Weight Growth Factor [-] (Get this value from the Iteration_Method_tool.xcl file)
         self.n_prop = 0.95      #Propeller efficiency [-]
@@ -23,6 +26,15 @@ class Parameters1:
         self.n_pmad = 0.9       #PMDAD efficiency [-]
         self.n_cooling = 0.4    #Cooling efficiency [-]
         self.n_fuelcell = 0.65  #Fuel cell efficiency [-]
+
+        #Noise Parameters
+        self.Pto = 1100000      #Take-off Power [W]
+        self.Dp = 3.0           #Propeller diameter [m]
+        self.B = 5              #Number of blades per propeller [-]
+        self.nrpm = 2500        #Engine RPM [evolutions/minute]
+        self.c = 343            #Speed of sound [m/s]
+        self.N = 2              #Number of propellers [-]
+        self.r = 350            #Distance to observer [m]
 
 
 def Efficiency(a):
@@ -32,9 +44,9 @@ def Efficiency(a):
     return GI1
 
 def Recyclability(a):
-    pass
+    RC = 1
+    return RC
 
 design = Parameters1()
 
 g_1 = Efficiency(design)
-VErandering
