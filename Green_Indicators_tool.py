3b4cb6f4ad343f69ff14d06bc75fc9b21c52be49
DanielTimmermans <danieltimmermans1@gmail.com>
2021-05-31 17:53:13 +0200

    Noise formula

--- Green_Indicators_tool.py
+++ Green_Indicators_tool.py
@@ -24,11 +24,12 @@ def recycle(list):
 
 def Efficiency(a):
     S = a.S_wing + a.S_tail + a.S_fl + a.S_lg
-    CL = (a.CL_wing * a.S_wing + a.CL_tail * a.S_tail + a.CL_fl * a.S_fl) / S
+    CL = (a.CL_wing * a.S_wing + a.CL_tail * a.S_tail + a.CL_fl * a.S_fl) / (S-a.S_lg)
     C_D_0 = (a.CD0_wing * a.S_wing + a.CD0_tail * a.S_tail + a.CD0_fl * a.S_fl+ a.CD0_lg * a.S_lg) / S
-    C_D_i = (a.CL_wing ** 2) / (pi * a.A_wing* a.e_wing) + (a.CL_tail ** 2) / (pi * a.A_tail* a.e_tail)
+    C_D_i = ( a.S_wing*(a.CL_wing ** 2) / (pi * a.A_wing* a.e_wing) + a.S_tail * (a.CL_tail ** 2) / (pi * a.A_tail* a.e_tail))/(S - a.S_lg - a.S_fl)
     C_D = C_D_0 + C_D_i
     GI_2 = CL / C_D * 1/(a.MTOW + a.dW * a.G) * a.n_prop * a.n_engine * a.n_pmad * a.n_cooling * a.n_fuelcell
+    print(CL)
     return GI_2
 
 def Noise(a):
@@ -52,7 +53,7 @@ def tool(a, b, c, d):
     print("G3 Noise indicator: ")
     print("Option 1 = ", Noise(a), "[dB]")
     print("Option 2 = ", Noise(c), "[dB]")
-    print("Option 1 is ", Noise(a) / Noise(c), "relative to Option 2")
+    print("Option 1 is ", Noise(a) - Noise(c), "higher relative to Option 2")
 # -- Parameter List -- #
 
 
@@ -60,22 +61,22 @@ def tool(a, b, c, d):
 class Parameters1:
     def __init__(self):
         #Efficiency Indicator Parameters option 1
-        self.CL_wing = 1.3              # CL  wing [-] (Generally use cruise conditions)
-        self.CD0_wing = 0.012           # CD0 wing [-]
-        self.S_wing = 100               # Surface area wing [m^2]
-        self.A_wing = 10                # Aspect ratio [-]
+        self.CL_wing = .55             # CL  wing [-] (Generally use cruise conditions)
+        self.CD0_wing = 0.006           # CD0 wing [-]
+        self.S_wing = 45               # Surface area wing [m^2]
+        self.A_wing = 9                # Aspect ratio [-]
         self.e_wing = 0.8               # Oswald effiency wing [-]
 
         #Tail option 1
-        self.CL_tail = 1.3              # CL  tail [-] (Generally use cruise conditions)
-        self.CD0_tail = 0.008           # CD0 tail [-]
-        self.S_tail = 50                # Surface area tail [m^2]
-        self.A_tail = 10                # Aspect ratio [-]
+        self.CL_tail = 0.55            # CL  tail [-] (Generally use cruise conditions)
+        self.CD0_tail = 0.006           # CD0 tail [-]
+        self.S_tail = 9.88 * (45/35.18)              # Surface area tail [m^2] (sized with same A, S and chord of L-410)
+        self.A_tail = 4.61 * (9/11.45)                # Aspect ratio [-]
         self.e_tail = 0.8               # Oswald effiency tail [-]
 
         #Fuselage option 1
-        self.CL_fl = 1.3                # CL fuselage [-] (Generally use cruise conditions)
-        self.CD0_fl = 0.01              # CD0 fuselage [-]
+        self.CL_fl = 0.001               # CL fuselage [-] (Generally use cruise conditions)
+        self.CD0_fl = 0.06              # CD0 fuselage [-]
         self.S_fl = 10                  # Surface area fuselage  [m^2]
 
         #Landing Gear option 1
@@ -84,22 +85,23 @@ class Parameters1:
 
         # General Parameters option 1
         self.MTOW = 8618                # Maximum Take-off Weight [kg]
-        self.dW = -200                  #Extra added weight [kg] (use negative value for weight reduction)
-        self.G = 4.73                   #Weight Growth Factor [-] (Get this value from the Iteration_Method_tool.xcl file)
-        self.n_prop = 0.95              #Propeller efficiency [-]
-        self.n_engine = 0.93            #Engine efficiency [-]
+        self.dW = 0                     #Extra added weight [kg] (use negative value for weight reduction)
+        self.G = 4.75                   #Weight Growth Factor [-] (Get this value from the Iteration_Method_tool.xcl file)
+        self.n_prop = 0.85              #Propeller efficiency [-]
+        self.n_engine = 0.9            #Engine efficiency [-]
         self.n_pmad = 0.9               #PMAD efficiency [-]
         self.n_cooling = 0.4            #Cooling efficiency [-]
-        self.n_fuelcell = 0.65          #Fuel cell efficiency [-]
+        self.n_fuelcell = 0.5          #Fuel cell efficiency [-]
 
         #Noise Parameters option 1
-        self.Pto = 1100000              #Take-off Power [W]
-        self.Dp = 3.0                   #Propeller diameter [m]
-        self.B = 5                      #Number of blades per propeller [-]
-        self.nrpm = 2500                #Engine RPM [evolutions/minute]
+        W_ac_L410 = 8618 / 6600
+        self.Pto = 634000 * 2 * (8618/6600)               #Take-off Power [W] (For now from l410 http://www.let.cz/documents/L410NG.pdf)
+        self.Dp = 2.3 * (8618/6600)                   #Propeller diameter [m] (For now from l410)
+        self.B = 5                      #Number of blades per propeller [-]    (For now from l410)
+        self.nrpm = 1950               #Engine RPM [evolutions/minute]   (For now from l410 http://www.let.cz/documents/L410NG.pdf)
         self.c = 343                    #Speed of sound [m/s]
         self.N = 2                      #Number of propellers [-]
-        self.r = 350                    #Distance to observer [m]
+        self.r = 1                      #Distance to observer [m]    (Might be set to 75m according to ICEO)
 
 class Parameters2:
     def __init__(self):
