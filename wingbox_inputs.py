917aaa234c7b412068885ab0ca624b43bf4de47d
dvlot <58387820+dvlot@users.noreply.github.com>
2021-06-08 12:12:58 +0200

    Wingbox inputs file

---
+++ wingbox_inputs.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 11:56:14 2021
+
+@author: DickD_000
+"""
+#Aerodynamic inputs
+L_D = 9.64
+V_cruise = 90
+
+
+#Main wing
+b_wing = 20
+S_w = 45
+taper_wing = 0.5
+    #Root dimensions of the wingbox
+h_wing = 0.4
+w_wing = 1.5
+
+
+
+#Vertical tail
+b_vert = 3
+taper_vert = 0.5
+    #Root dimensions of the wingbox
+h_vert = 0.1
+w_vert = 0.5
+
+
+
+#Horizontal tail
+b_hor = 3
+taper_hor = 0.5
+    #Root dimensions of the wingbox
+h_hor = 0.1
+w_hor = 0.5
