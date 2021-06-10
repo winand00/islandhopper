3403f76c9ab383df2d347922e97ef225460066b0
Olav <olav_730@hotmail.com>
2021-06-10 14:14:44 +0200

    Landing gear design

--- Landing_gear_design.py
+++ Landing_gear_design.py
@@ -2,22 +2,22 @@ from math import *
 from CGloadingdiagram import max_xcg_Hopper_nose
 
 MTOW = 8618
-Tip_over = 15           #tip over angle
-Scrape_angle = 13       #scrape angle
-overturn = 55           #overturn angle
+Tip_over = 15                                           #tip over angle
+Scrape_angle = 13                                       #scrape angle
+overturn = 55                                           #overturn angle
 Phi_clearance = 5
 
-Height_fuselage = 1.91  #height fuselage
-Height_mainlg = (2/3)*Height_fuselage+0.5
+Height_fuselage = 1.91                                  #height fuselage
+Height_mainlg = (2/3)*Height_fuselage+0.5               #height of cg to ground #estimated at 2/3 of fuselage height + 0.5 landing gear height
 wingspan = 20
 
-Tip_height = Height_fuselage
+Tip_height = Height_fuselage                            #estimated with 0.5 meter deflection downwards during taxi operation
 Prop_dia = 2.4
-Prop_height = Tip_height - Prop_dia/2
-Engine_yloc = 10
-n_m = 2                 #number of main landing gears
-Pn = 8                  #Percentge of weight on the nose wheel
-Lm = sin(radians(Tip_over))*Height_mainlg                  #Length of main landing gear to CG
+Prop_height = Tip_height - Prop_dia/2                   #Estimated to be worst case: with deflection of tip. located at center of the wing
+Engine_yloc = 10                                        #lateral engine placement.
+n_m = 2                                                 #number of main landing gears
+Pn = 10                                                 #Percentge of weight on the nose wheel
+Lm = sin(radians(Tip_over))*Height_mainlg               #Length of main landing gear to CG
 
 
 Ln = (100-Pn)/Pn*Lm
@@ -26,13 +26,14 @@ Pm_total = MTOW*(1-(Pn/100))/n_m
 
 Lgmain_xloc = Lm+max_xcg_Hopper_nose
 Lgnose_xloc = max_xcg_Hopper_nose-Ln
+
 Lgmain_yloc1 = (Ln+Lm)/(sqrt((Ln**2*tan(radians(overturn))**2)/Height_mainlg-1))    #Tip over distance
 Lgmain_yloc2 = wingspan/2-Tip_height/tan(radians(Phi_clearance))                    #Tip clearance distance
 Lgmain_yloc3 = Engine_yloc-Prop_height/tan(radians(Phi_clearance))                  #Prop clearance distance
 print(Tip_height)
 print(Lgmain_xloc)
-print(Lgnose_xloc)
-print(Lgmain_yloc1,Lgmain_yloc2,Lgmain_yloc3)
+print(Lgnose_xloc)                                      #nose wheel location from nose of the aircraft
+print(Lgmain_yloc1,Lgmain_yloc2,Lgmain_yloc3)           #prints the 3 critical y placements from center.
 
 
 
