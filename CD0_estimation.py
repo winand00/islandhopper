d2dcc68dcfb436ed3f75981813fbd5742e3226df
DanielTimmermans <danieltimmermans1@gmail.com>
2021-06-11 14:23:04 +0200

    .

--- CD0_estimation.py
+++ CD0_estimation.py
@@ -1,11 +1,19 @@
+from math import *
 
-def get_S_w(S_w_w,S_w_f,S_w_t,S_w_n)
+def get_S_w(S_w_w,S_w_f,S_w_t,S_w_n):
     S_w = S_w_w + S_w_f + S_w_t + S_w_n
     return S_w
 
-def get S_wing(b,c_r,Lambda,eta_c,Lambda_c)
-    S_wing = 0.5 * b * c_r * (Lambda*(1-eta_c) + eta_c + Lambda_c)
+def get_S(b,c_r,c_t,y_c,c_c):
+    Lambda = c_t/c_r
+    Lambda_c = c_c/c_r
+    eta_c = y_c * (b/2)
+    S = 0.5 * b * c_r * (Lambda*(1-eta_c) + eta_c + Lambda_c)
+    return S
 
+def get_S_e_w(S,c_r,d,):
+    S_e_w = S - c_r * d
+    S_e_w_2
 
 def get_S_w_w()
 
@@ -16,3 +24,5 @@ S_w = 1     #Total wetted area in clean configuration
 S_w_w = 1   #Wing wetted area
 S_w_f = 1   #Fuselage wetted area
 S_w_n = 1   #Nacelle wetted area
+
+#Wing wetted area
